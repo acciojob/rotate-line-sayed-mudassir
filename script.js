e542b9bd49ff74722f5781176a006f34c7aa285c
sayed mudassir <sayedmuddassir68@gmail.com>
2024-05-30 01:13:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const line = document.getElementById("line");
+let angle = 0;
+function rotate(){
+    angle = (angle + 2) % 360; // Ensure angle stays within 0-359 degrees
+    line.style.transform = `translate(-50%, 50%) rotate(${angle}deg)`;
+    
+}
+setInterval(rotate,20);
